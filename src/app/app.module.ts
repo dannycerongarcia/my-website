1a58561e42bca56085ee4ec047df282dc7bbc9bf
dannyceron94 <dannyceron94@gmail.com>
2020-09-16 17:52:34 -0700

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Background } from './background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Background,
   ],
   imports: [
     BrowserModule
---
+++ src/app/background.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core'
+@Component({
+    selector:'background',
+    template:`
+    <h1> Welcome to Danny's site </h1>
+    <div class="row"> 
+        <div class="col"> YEET 1</div>
+        <div class="col"> YEET 2</div>
+        <div class="col"> YEET 3</div>
+    </div>
+    `
+})
+export class Background{
+    title="Danny's Website"
+}
